ebfe2ce13bc5513541d9c575de7828821907fe4e
Fatma Oymak <sugunum@gmail.com>
2020-07-08 18:25:27 +0300

    some change

--- script.js
+++ script.js
@@ -7,6 +7,33 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+
+let table = document.createElement("table");
+rootElem.appendChild(table);
+
+//table.innerHTML = "<tr><td>hi</td></tr>"
+
+for(let i = 0; i < episodeList.length; i++){
+let row = document.createElement("tr")
+let cell = document.createElement("td")
+//cell.innerText = episodeList[i].name
+
+let name = document.createElement("p");
+name.innerText = episodeList[i].name
+let image = document.createElement("img")
+image.src = episodeList[i].image.medium
+
+
+
+cell.appendChild(name)
+cell.appendChild(image)
+row.appendChild(cell)
+table.appendChild(row)
+
+  console.log(episodeList[i].name);
+}
 }
 
 window.onload = setup;
+
