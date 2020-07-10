5a4d02a50ec418f6e6557a321b85ffcdc2fbe094
Fatma Oymak <sugunum@gmail.com>
2020-07-11 01:31:13 +0300

    saturday class before change

--- script.js
+++ script.js
@@ -19,15 +19,39 @@ let row = document.createElement("tr")
 let cell = document.createElement("td")
 //cell.innerText = episodeList[i].name
 
+/*let number
+let formattedNmuber = ("0"+number).slice(-2)
+formattedNumber = document.createElement("number")
+number.innerText = episodeList[i].number
+
+
+"abcd".slice(-1) == "d"
+8:14
+"abcd".slice(-2) == "cd"
+8:14
+"abcd".slice(-3) == "bcd"
+S + season_number + E + episode_number
+*/
+let seasonum
+let seasonNumber = ("0"+seasonum).slice(-2)
+seasonNumber = document.createElement("season")
+seasonum.innerText = episodeList[i].season
+
 let name = document.createElement("p");
 name.innerText = episodeList[i].name
+
 let image = document.createElement("img")
 image.src = episodeList[i].image.medium
 
+let summary = document.createElement("p")
+summary.innerText = episodeList[i].summary
 
 
+//cell.appendChild(number)
+cell.appendChild(season)
 cell.appendChild(name)
 cell.appendChild(image)
+cell.appendChild(summary)
 row.appendChild(cell)
 table.appendChild(row)
 
